d85aaa1daf143df81ac361d1d5ec5bf950b28503
ThomasGoerttler <thomas.goerttler@gmail.com>
2013-06-20 09:56:43 +0200

    Change text

--- UpdateYourBrowser.js
+++ UpdateYourBrowser.js
@@ -7,7 +7,7 @@
 			'<style type="text/css">'+
 				'#UpdateYourBrowser{z-index:2147483647;position:absolute;left:25%;top:25%;}'+
 			'</style>'+
-				'<img src="https://raw.github.com/adius/UpdateYourBrowser/master/img/default_en.png" alt="Update your browser!" width="640px" height="340px" usemap="#linkmap" />'+
+				'<img src="https://raw.github.com/adius/UpdateYourBrowser/master/img/default_en.png" alt="Update your browser! It will be an atvantagde for you" width="640px" height="340px" usemap="#linkmap" />'+
 				'<map style="cursor: pointer" name="linkmap" id="linkmap">'+
 					'<area shape="circle" coords="622,18,16" onclick="getElementById(\'UpdateYourBrowser\').style.display = \'none\';" title="Close Window" alt="Close Window" />'+
 					'<area shape="rect" coords="60,180,170,320" href="http://mozilla.com/" target="_blank" title="Download Firefox" alt="Download Firefox" />'+
